4aefe2b2809215158bee28a75bcce29b8ab07e31
Mufleh <muflehawan@gmail.com>
2024-04-30 13:28:23 +0100

    cart and buying

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -1,7 +1,53 @@
-﻿namespace MyCheeseShop.Model
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+
+
+namespace MyCheeseShop.Model
 {
     public class ShoppingCart
     {
-        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public event Action? OnCartUpdated;
+        public List<CartItem> _items;
+
+        public ShoppingCart()
+        {
+            _items = [];
+        }
+
+        public void AddItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item == null)
+                _items.Add(new CartItem { Cheese = cheese, Quantity = quantity });
+
+            else
+                item.Quantity += quantity;
+
+
+
+            OnCartUpdated?.Invoke();
+
+
+
+
+
+
+
+
+
+        }
+        public IEnumerable<CartItem> GetItems()
+        {
+            return _items;
+        }
+
+
+        public int GetQuantity(Cheese cheese)
+        {
+           
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            return item?.Quantity ?? 0;
+        }
     }
 }
